f65b2dd48a7b258e6faa7ef13e4038387073a56a
super-web-dev <shopifydeveloper758@gmail.com>
2019-07-28 04:30:29 +1000

    'ugdate-1'

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,39 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="hello card">
+    <h1> To do: </h1>
+    <div class="container">
+      <div id="tasks">
+        <div class="col-md-6">
+          
+          <div v-for="id in tasks.length" :key="id">
+
+            <p class="form-inline" :class="!tasks[id-1].completed ? 'complete': 'incomplete'">
+              <input class="form-control" 
+                     v-model="tasks[id-1].body"
+                     @click="incompleteTask(tasks[id-1])">
+
+              <button id="toggleComplete" 
+                      class="btn glyphicon" 
+                      @click="toggleComplete(tasks[id-1])"></button>
+
+              <button id="deleteTask" 
+                      class="btn glyphicon"
+                      @click="deleteTask(id-1)"></button>
+            </p>
+          </div>
+          <form @submit="addTask">
+            <div class="form-inline">
+              <input type="text" 
+                     class="form-control" placeholder="I need to..." 
+                     v-model="newTask" 
+                     />
+              <button type="submit" class="btn  btn-primary" >Add</button>
+            </div>
+          </form>
+        </div>
+
+      </div>
+    </div>
   </div>
 </template>
 
@@ -35,24 +42,102 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() { return {
+      tasks: [{
+        "body": "Learn Vue.js",
+        "completed": false
+      }],
+      newTask: ''
+    }
+  },
+  computed: {
+    completedTasks: function(){
+      return this.tasks.filter(function(task){
+        return task.completed;
+      });
+    },
+    remainingTasks: function(){
+      return this.tasks.filter(function(task){
+        return ! task.completed;
+      });
+    },
+  },
+  methods: {
+    addTask: function(e) {
+      e.preventDefault();
+      if (this.newTask == '') return;
+      this.tasks.push({
+        body: this.newTask,
+        completed: false
+      });
+      this.newTask = ''
+    },
+    deleteTask: function(task_id){
+      this.$delete(this.tasks, task_id);
+    },
+    toggleComplete: function(task){
+      task.completed = ! task.completed;
+    },
+    incompleteTask: function(task){
+      task.completed = false;
+    },
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style lang="scss" scoped>
+input, pre {
+  font-size: 20px !important;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.complete{
+  #toggleComplete{
+    color: #fff !important;
+    background-color: #5bc0de;
+    border-color: #46b8da;
+    &:before{
+      content: "\e013"
+    }
+  }
+  #deleteTask{
+    color: #fff !important;
+    background-color: #d9534f;
+    border-color: #d43f3a;
+    &:before{
+      content: "\e014"
+    }
+  }
+  input{
+      border: none;
+      box-shadow: none;   
+    &:focus{
+      box-shadow: none;   
+    }
+  }
+  
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.incomplete{
+  #toggleComplete{
+    color: #fff !important;
+    &:before{
+      content: "\2212"
+    }
+  }
+  #deleteTask{
+    color: #fff !important;
+    background-color: #d9534f;
+    border-color: #d43f3a;
+    &:before{
+      content: "\e014"
+    }
+  }
+  input{
+      text-decoration: line-through;
+      border: none;
+      box-shadow: none;
+      color: grey;
+    
+  }
 }
 </style>
